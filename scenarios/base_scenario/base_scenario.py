0898ac2b9c4115b5be62182e70b5a7ccf03dd1f3
Julius Wallnöfer <33934646+jwallnoefer@users.noreply.github.com>
2023-07-13 14:48:56 +0200

    workable base scenario

--- scenarios/base_scenario/base_scenario.py
+++ scenarios/base_scenario/base_scenario.py
@@ -17,6 +17,7 @@ from common.consts import (
     SPEED_OF_LIGHT_IN_OPTICAL_FIBER as C,
     ATTENUATION_LENGTH_IN_OPTICAL_FIBER,
 )
+from multipartite_requsim.event import ConnectBellsToGHZEvent
 
 
 def construct_dephasing_noise_channel(dephasing_time):
@@ -59,9 +60,9 @@ class CentralMultipartyProtocol(Protocol):
     def __init__(
         self, world, central_station, stations, sources, communication_speed=C
     ):
-
         self.central_station = central_station
         self.stations = stations
+        self._N = len(stations)
         self.sources = sources
         assert len(self.stations) == len(self.sources)
         self.num_parties = len(self.stations)
@@ -101,103 +102,67 @@ class CentralMultipartyProtocol(Protocol):
             )
         )
 
-    def _eval_pair(self, long_range_pair):
+    def _eval_ghz(self, long_range_ghz: MultiQubit):
         comm_distance = np.max(
-            [
-                distance(self.stations[0], self.stations[-2]),
-                distance(self.stations[-1], self.stations[1]),
-            ]
+            [distance(self.central_station, station) for station in self.stations]
         )
         # comm_distance is simple upper limit for swapping communication
         comm_time = comm_distance / self.communication_speed
-        for qubit in long_range_pair.qubits:
+
+        for qubit in long_range_ghz.qubits:
             for time_dependent_noise in qubit._time_dependent_noises:
                 qubit.apply_noise(time_dependent_noise, t=comm_time)
         self.time_list += [self.world.event_queue.current_time + comm_time]
-        self.state_list += [long_range_pair.state]
+        self.state_list += [long_range_ghz.state]
         return
 
-    # def _eval_ghz(self, long_range_ghz: MultiQubit):
-    #     comm_distance = np.max(
-    #         [
-    #             distance(self.central_station, station) for station in self.stations
-    #         ]
-    #     )
-    #     # comm_distance is simple upper limit for swapping communication
-    #     comm_time = comm_distance / self.communication_speed
-    #
-    #     for qubit in long_range_ghz.qubits:
-    #         for time_dependent_noise in qubit._time_dependent_noises:
-    #             qubit.apply_noise(time_dependent_noise, t=comm_time)
-    #     self.time_list += [self.world.event_queue.current_time + comm_time]
-    #     self.state_list += [long_range_ghz.state]
-    #     return
-
     def check(self, message=None):
-        pairs_by_station = []
-        num_pairs_by_station = []
-        num_pairs_scheduled_by_station = []
-        for station in self.stations:
-            pairs = self._get_pairs_between_stations(station, self.central_station)
-            pairs_by_station.append(pairs)
-            num_pairs = len(pairs)
-            num_pairs_by_station.append(num_pairs)
-            num_pairs_scheduled = len(
-                self._get_pairs_scheduled(station, self.central_station)
-            )
-            num_pairs_scheduled_by_station.append(num_pairs_scheduled)
+        pairs_by_station = [
+            self._get_pairs_between_stations(station, self.central_station)
+            for station in self.stations
+        ]
+        num_pairs_by_station = [len(pair_list) for pair_list in pairs_by_station]
+        num_pairs_scheduled_by_station = [
+            len(self._get_pairs_scheduled(station, self.central_station))
+            for station in self.stations
+        ]
 
-        left_pairs = self._get_pairs_between_stations(
-            self.stations[0], self.central_station
-        )
-        num_left_pairs = len(left_pairs)
-        num_left_pairs_scheduled = len(
-            self._get_pairs_scheduled(self.stations[0], self.central_station)
-        )
-        right_pairs = self._get_pairs_between_stations(
-            self.stations[1], self.central_station
-        )
-        num_right_pairs = len(right_pairs)
-        num_right_pairs_scheduled = len(
-            self._get_pairs_scheduled(self.stations[1], self.central_station)
-        )
-        long_distance_pairs = self._get_pairs_between_stations(
-            self.stations[0], self.stations[1]
-        )
+        try:
+            long_distance_ghzs = self.world.world_objects[f"{self._N}-qubit MultiQubit"]
+        except KeyError:
+            long_distance_ghzs = []
 
         # STEP 1: For each link, if there are no pairs established and
         #         no pairs scheduled: Schedule a pair.
-        if num_left_pairs + num_left_pairs_scheduled == 0:
-            self.sources[0].schedule_event()
-        if num_right_pairs + num_right_pairs_scheduled == 0:
-            self.sources[1].schedule_event()
+        for i, station in enumerate(self.stations):
+            if num_pairs_by_station[i] + num_pairs_scheduled_by_station[i] == 0:
+                self.sources[i].schedule_event()
 
-        # STEP 2: If both links are present, do entanglement swapping.
-        if num_left_pairs == 1 and num_right_pairs == 1:
-            left_pair = left_pairs[0]
-            right_pair = right_pairs[0]
-            ent_swap_event = EntanglementSwappingEvent(
+        # STEP 2: If all links are present, merge them into a GHZ state.
+        if np.all([num_pairs == 1 for num_pairs in num_pairs_by_station]):
+            pairs = [pair_list[0] for pair_list in pairs_by_station]
+            connect_event = ConnectBellsToGHZEvent(
                 time=self.world.event_queue.current_time,
-                pairs=[left_pair, right_pair],
+                pairs=pairs,
                 station=self.central_station,
             )
-            self.world.event_queue.add_event(ent_swap_event)
+            self.world.event_queue.add_event(connect_event)
 
-        # STEP 3: If a long range pair is present, save its data and delete
+        # STEP 3: If a long range GHZ is present, save its data and delete
         #         the associated objects.
-        if long_distance_pairs:
-            for pair in long_distance_pairs:
-                self._eval_pair(pair)
-                for qubit in pair.qubits:
+        if long_distance_ghzs:
+            for ghz in long_distance_ghzs:
+                self._eval_ghz(ghz)
+                for qubit in ghz.qubits:
                     qubit.destroy()
-                pair.destroy()
+                ghz.destroy()
 
 
 def alpha_of_eta(eta, p_d):
     return eta * (1 - p_d) / (1 - (1 - eta) * (1 - p_d) ** 2)
 
 
-def run(distance_from_central, max_iter, params):
+def run(distance_from_central, num_parties, max_iter, params):
     allowed_params = ["P_LINK", "T_P", "P_D", "F_INIT", "COMMUNICATION_SPEED", "T_DP"]
     for key in params:
         if key not in allowed_params:
@@ -215,6 +180,8 @@ def run(distance_from_central, max_iter, params):
             e.__traceback__
         )
     L_ATT = ATTENUATION_LENGTH_IN_OPTICAL_FIBER
+
+    N = num_parties
 
     @lru_cache()  # caching only makes sense with stationary stations and sources
     def state_generation(source):
@@ -268,7 +235,6 @@ def run(distance_from_central, max_iter, params):
         random_num = np.random.geometric(eta_effective)
         return random_num * trial_time
 
-    N = 3
     # setup scenario
     world = World()
 
@@ -278,13 +244,19 @@ def run(distance_from_central, max_iter, params):
         memory_noise=construct_dephasing_noise_channel(dephasing_time=T_DP),
     )
 
+    angles = np.linspace(0, 2 * np.pi, num=N, endpoint=False)
     other_stations = [
         Station(
             world,
-            position=np.array([0, distance_from_central]),
+            position=np.array(
+                [
+                    distance_from_central * np.cos(phi),
+                    distance_from_central * np.sin(phi),
+                ]
+            ),
             memory_noise=construct_dephasing_noise_channel(dephasing_time=T_DP),
         )
-        for i in range(N)
+        for phi in angles
     ]
 
     sources = [
@@ -306,54 +278,55 @@ def run(distance_from_central, max_iter, params):
         communication_speed=CS,
     )
     protocol.setup()
+
+    # main loop
     current_message = None
     while len(protocol.time_list) < max_iter:
-        world.print_status()
-        input()
+        # world.print_status()
+        # input()
         protocol.check(current_message)
-        world.print_status()
-        input()
+        # world.print_status()
+        # input()
         current_message = world.event_queue.resolve_next_event()
 
     return protocol
 
 
-# def ghz_fidelity(data: pd.DataFrame, num_parties: int):
-#     z0s = [mat.z0] * num_parties
-#     z0s = mat.tensor(*z0s)
-#     z1s = [mat.z1] * num_parties
-#     z1s = mat.tensor(*z1s)
-#     ghz_psi = 1 / np.sqrt(2) * (z0s + z1s)
-#
-#     states = data["state"]
-#     fidelity_list = np.real_if_close(
-#         [
-#             np.dot(np.dot(mat.H(ghz_psi), state), ghz_psi)[0, 0]
-#             for state in states
-#         ]
-#     )
-#     fidelity = np.mean(fidelity_list)
-#     fidelity_std_err = np.std(fidelity_list) / np.sqrt(len(fidelity_list))
-#     return fidelity, fidelity_std_err
+def ghz_fidelity(data: pd.DataFrame, num_parties: int):
+    z0s = [mat.z0] * num_parties
+    z0s = mat.tensor(*z0s)
+    z1s = [mat.z1] * num_parties
+    z1s = mat.tensor(*z1s)
+    ghz_psi = 1 / np.sqrt(2) * (z0s + z1s)
+
+    states = data["state"]
+    fidelity_list = np.real_if_close(
+        [np.dot(np.dot(mat.H(ghz_psi), state), ghz_psi)[0, 0] for state in states]
+    )
+    fidelity = np.mean(fidelity_list)
+    fidelity_std_err = np.std(fidelity_list) / np.sqrt(len(fidelity_list))
+    return fidelity, fidelity_std_err
 
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
     max_iter = 1e2
-    lengths = np.linspace(1e3, 100e3, num=20)
+    num_parties = 4
+    lengths = np.linspace(1e3, 30e3, num=20)
     fidelities = []
     fidelity_std_err = []
     for length in lengths:
         print(f"{length/1000:.2f}")
         res = run(
             distance_from_central=length,
+            num_parties=num_parties,
             max_iter=max_iter,
             params={"P_LINK": 0.01, "T_DP": 100e-3, "F_INIT": 0.99},
         )
-        evaluation = standard_bipartite_evaluation(data_frame=res.data)
-        fidelities.append(evaluation[1])
-        fidelity_std_err.append(evaluation[2])
+        evaluation = ghz_fidelity(data=res.data, num_parties=num_parties)
+        fidelities.append(evaluation[0])
+        fidelity_std_err.append(evaluation[1])
     plt.errorbar(lengths / 1000, fidelities, yerr=fidelity_std_err, fmt="o", ms=3)
     plt.xlabel("distance to central station [km]")
     plt.ylabel("average fidelity F")
