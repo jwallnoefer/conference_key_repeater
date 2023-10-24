64a36e5a409cf6a55fc4c4ecb08cdfb46e1d1a3f
Julius Wallnöfer <33934646+jwallnoefer@users.noreply.github.com>
2023-10-24 13:45:46 +0200

    initial work on merging event

--- tests/multipartite_requsim_test/GHZ_events_test.py
+++ tests/multipartite_requsim_test/GHZ_events_test.py
@@ -1,5 +1,5 @@
 import numpy as np
-from multipartite_requsim.event import ConnectBellsToGHZEvent
+from multipartite_requsim.event import ConnectBellsToGHZEvent, MergeToGHZEvent
 from requsim.world import World
 from requsim.quantum_objects import Station, Pair, MultiQubit
 from requsim.events import EntanglementSwappingEvent
@@ -86,3 +86,22 @@ def test_two_pairs_equivalent_to_ent_swap():
     new_pair = return_value["output_pair"]
     expected_state = new_pair.state
     assert np.allclose(output_state, expected_state)
+
+
+def test_merging_produces_ghz_in_ideal():
+    for num_parties in range(2, 9):
+        world = World()
+        central_station = Station(world=world, position=0)
+        stations = [Station(world=world, position=100) for i in range(num_parties)]
+        pairs = []
+        for station in stations:
+            qubits = [central_station.create_qubit(), station.create_qubit()]
+            new_pair = Pair(world=world, qubits=qubits, initial_state=rho_phiplus)
+            pairs += [new_pair]
+        event = MergeToGHZEvent(time=0, inputs=pairs, station=central_station)
+        world.event_queue.add_event(event)
+        return_value = world.event_queue.resolve_next_event()
+        assert len(world.world_objects["Pair"]) == 0
+        assert len(world.world_objects[f"{num_parties+1}-qubit MultiQubit"]) == 1
+        output = return_value["output_state"]
+        assert np.allclose(output.state, n_qubit_ghz_dm(num_parties + 1))
