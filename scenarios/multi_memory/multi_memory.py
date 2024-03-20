430ae1b26ad22602f65ddf0ebca5de3266edc75f
Julius Wallnöfer <33934646+jwallnoefer@users.noreply.github.com>
2024-03-20 15:57:22 +0100

    fix in formatting

--- scenarios/multi_memory/multi_memory.py
+++ scenarios/multi_memory/multi_memory.py
@@ -165,7 +165,9 @@ class CentralMultipartyProtocol(Protocol):
         num_to_merge = np.min(num_pairs_by_station)
         if num_to_merge:
             # merge the first only since this will be checked again anyways
-            pairs = [pair_list[-1] for pair_list in pairs_by_station] # merge newest pairs if possible
+            pairs = [
+                pair_list[-1] for pair_list in pairs_by_station
+            ]  # merge newest pairs if possible
             try:
                 next(
                     filter(
