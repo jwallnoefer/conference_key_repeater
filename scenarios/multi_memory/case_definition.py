c513a16d8c128e8295557bed97b00ea57fb2c891
jmemmen <janka.memmen@tu-berlin.de>
2024-08-20 14:25:56 +0200

    new cases

--- scenarios/multi_memory/case_definition.py
+++ scenarios/multi_memory/case_definition.py
@@ -3574,7 +3574,7 @@ case_specification_4 = {
 }
 cases.append(case_specification_4)
 
-case_name = "P_sym150_per_TCUT_5mem"
+case_name = "P_sym150_per_TCUT_1mem"
 num_parts = 64
 max_iter = 10**5
 num_parties = 4
@@ -3680,7 +3680,7 @@ cases.append(case_specification_3)
 
 case_specification_4 = {
     "name": case_name,
-    "subcase_name": "measure_central",
+    "subcase_name": "measure_outer",
     "num_parts": num_parts,
     "index": T_CUTS,
     "case_args": {
@@ -3840,6 +3840,562 @@ case_specification_4 = {
     },
 }
 cases.append(case_specification_4)
+
+
+
+### 2D case, new input distance from central: list of floats
+case_name = "P_unis_T2_m_nocut"
+num_parts_x = 20
+num_parts_y = 20
+num_parts = num_parts_x*num_parts_y
+max_iter = 10**3
+num_parties = 4
+# distances between Düsseldorf and Köln, Wuppertal and Duisburg
+distance_from_central = [31e3, 25e3, 27e3]
+# distance between Düsseldorf and Siegen
+distance_A = 76e3
+
+T_CUT = None
+
+
+ms = np.linspace(1, 20, num=num_parts_x)  # x-axis
+T_2s = np.logspace(-3, 1, num=num_parts_y)  # y-axis
+
+param_matrix = np.array(np.meshgrid(ms, T_2s)).T.reshape(-1, 2)
+
+
+case_specification_1 = {
+    "name": case_name,
+    "subcase_name": "distribute_central",
+    "num_parts": num_parts,
+    "index": list(range(num_parts)),
+    "case_args": {
+        part: {
+            "distance_from_central": distance_from_central,
+            "distance_A": distance_A,
+            "num_parties": num_parties,
+            "max_iter": max_iter,
+            "params": {
+                "P_LINK": 1,
+                "F_INIT": 0.99,
+                "T_P": 1e-6,
+                "P_D": 1e-6,
+                "T_DP": param_matrix[part][1],
+                "T_CUT": T_CUT,
+            },
+            "num_memories": int(param_matrix[part][0]),
+            "mode": "distribute",
+            "source_position": "central",
+        }
+        for part in range(num_parts)
+    }
+}
+
+
+cases.append(case_specification_1)
+
+### 2D case, new input distance from central: list of floats
+case_name = "P_70_N_m_nocut"
+num_parts_x = 6
+num_parts_y = 5
+num_parts = num_parts_x*num_parts_y
+max_iter = 10**2
+# other distances
+distance_B = 70e3
+# distance to A link
+distance_A = 70e3
+
+T_CUT = None
+
+
+Ns = np.linspace(4, 9, num=num_parts_x)  # x-axis
+ms = np.linspace(1, 20, num=num_parts_y)  # y-axis
+
+param_matrix = np.array(np.meshgrid(Ns, ms)).T.reshape(-1, 2)
+
+case_specification_1 = {
+    "name": case_name,
+    "subcase_name": "distribute_central",
+    "num_parts": num_parts,
+    "index": list(range(num_parts)),
+    "case_args": {
+        part: {
+            "distance_from_central": [distance_B] * int(param_matrix[part][0]-1),
+            "distance_A": distance_A,
+            "num_parties": param_matrix[part][0],
+            "max_iter": max_iter,
+            "params": {
+                "P_LINK": 1,
+                "F_INIT": 0.99,
+                "T_P": 1e-6,
+                "P_D": 1e-6,
+                "T_DP": 1,
+                "T_CUT": T_CUT,
+            },
+            "num_memories": param_matrix[part][0],
+            "mode": "distribute",
+            "source_position": "central",
+        }
+        for part in range(num_parts)
+    }
+}
+
+
+cases.append(case_specification_1)
+
+case_name = "P_sym150_per_TCUT_1mem_v2"
+num_parts = 64
+max_iter = 10**4
+num_parties = 4
+length = 150e3
+base_params = {
+    "P_LINK": 1,
+    "F_INIT": 0.99,
+    "T_P": 1e-6,  # preparation time, consistent with MHz entangled pair source
+    "P_D": 1e-6,  # dark count probability
+    "T_DP": 1,  # dephasing time
+}
+num_memories = 1
+
+
+
+T_CUTS = np.linspace(0.005, 0.5, num=num_parts)
+
+case_specification_1 = {
+    "name": case_name,
+    "subcase_name": "distribute_central",
+    "num_parts": num_parts,
+    "index": T_CUTS,
+    "case_args": {
+        part: {
+            "distance_from_central": [length] * int(num_parties-1),
+            "distance_A": length,
+            "num_parties": num_parties,
+            "max_iter": max_iter,
+            "params": {
+                "P_LINK": 1,
+                "F_INIT": 0.99,
+                "T_P": 1e-6,
+                "P_D": 1e-6,
+                "T_DP": 1,
+                "T_CUT": T_CUTS[part],
+            },
+            "num_memories": num_memories,
+            "mode": "distribute",
+            "source_position": "central",
+        }
+        for part in range(num_parts)
+    },
+}
+cases.append(case_specification_1)
+
+case_specification_2 = {
+    "name": case_name,
+    "subcase_name": "distribute_outer",
+    "num_parts": num_parts,
+    "index": T_CUTS,
+    "case_args": {
+        part: {
+            "distance_from_central": [length] * int(num_parties - 1),
+            "distance_A": length,
+            "num_parties": num_parties,
+            "max_iter": max_iter,
+            "params": {
+                "P_LINK": 1,
+                "F_INIT": 0.99,
+                "T_P": 1e-6,
+                "P_D": 1e-6,
+                "T_DP": 1,
+                "T_CUT": T_CUTS[part],
+            },
+            "num_memories": num_memories,
+            "mode": "distribute",
+            "source_position": "outer",
+        }
+        for part in range(num_parts)
+    },
+}
+cases.append(case_specification_2)
+
+
+case_specification_3 = {
+    "name": case_name,
+    "subcase_name": "measure_central",
+    "num_parts": num_parts,
+    "index": T_CUTS,
+    "case_args": {
+        part: {
+            "distance_from_central": [length] * int(num_parties - 1),
+            "distance_A": length,
+            "num_parties": num_parties,
+            "max_iter": max_iter,
+            "params": {
+                "P_LINK": 1,
+                "F_INIT": 0.99,
+                "T_P": 1e-6,
+                "P_D": 1e-6,
+                "T_DP": 1,
+                "T_CUT": T_CUTS[part],
+            },
+            "num_memories": num_memories,
+            "mode": "measure",
+            "source_position": "central",
+        }
+        for part in range(num_parts)
+    },
+}
+cases.append(case_specification_3)
+
+
+case_specification_4 = {
+    "name": case_name,
+    "subcase_name": "measure_outer",
+    "num_parts": num_parts,
+    "index": T_CUTS,
+    "case_args": {
+        part: {
+            "distance_from_central": [length] * int(num_parties - 1),
+            "distance_A": length,
+            "num_parties": num_parties,
+            "max_iter": max_iter,
+            "params": {
+                "P_LINK": 1,
+                "F_INIT": 0.99,
+                "T_P": 1e-6,
+                "P_D": 1e-6,
+                "T_DP": 1,
+                "T_CUT": T_CUTS[part],
+            },
+            "num_memories": num_memories,
+            "mode": "measure",
+            "source_position": "outer",
+        }
+        for part in range(num_parts)
+    },
+}
+cases.append(case_specification_4)
+
+
+case_name = "P_70_N_m_nocut_correct"
+num_parts_x = 6
+num_parts_y = 5
+num_parts = num_parts_x*num_parts_y
+max_iter = 10**2
+# other distances
+distance_B = 70e3
+# distance to A link
+distance_A = 70e3
+
+T_CUT = None
+
+
+Ns = np.linspace(4, 9, num=num_parts_x)  # x-axis
+ms = np.linspace(4, 20, num=num_parts_y)  # y-axis
+
+param_matrix = np.array(np.meshgrid(Ns, ms)).T.reshape(-1, 2)
+
+case_specification_1 = {
+    "name": case_name,
+    "subcase_name": "distribute_central",
+    "num_parts": num_parts,
+    "index": list(range(num_parts)),
+    "case_args": {
+        part: {
+            "distance_from_central": [distance_B] * int(param_matrix[part][0]-1),
+            "distance_A": distance_A,
+            "num_parties": param_matrix[part][0],
+            "max_iter": max_iter,
+            "params": {
+                "P_LINK": 1,
+                "F_INIT": 0.99,
+                "T_P": 1e-6,
+                "P_D": 1e-6,
+                "T_DP": 1,
+                "T_CUT": T_CUT,
+            },
+            "num_memories": param_matrix[part][0],
+            "mode": "distribute",
+            "source_position": "central",
+        }
+        for part in range(num_parts)
+    }
+}
+
+
+cases.append(case_specification_1)
+
+
+case_name = "P_70_N_m_nocut_correct_v2"
+num_parts_x = 6
+num_parts_y = 5
+num_parts = num_parts_x*num_parts_y
+max_iter = 10**2
+# other distances
+distance_B = 70e3
+# distance to A link
+distance_A = 70e3
+
+T_CUT = None
+
+
+Ns = np.linspace(4, 9, num=num_parts_x)  # x-axis
+ms = np.linspace(4, 20, num=num_parts_y)  # y-axis
+
+param_matrix = np.array(np.meshgrid(Ns, ms)).T.reshape(-1, 2)
+
+case_specification_1 = {
+    "name": case_name,
+    "subcase_name": "distribute_central",
+    "num_parts": num_parts,
+    "index": list(range(num_parts)),
+    "case_args": {
+        part: {
+            "distance_from_central": [distance_B] * int(param_matrix[part][0]-1),
+            "distance_A": distance_A,
+            "num_parties": int(param_matrix[part][0]),
+            "max_iter": max_iter,
+            "params": {
+                "P_LINK": 1,
+                "F_INIT": 0.99,
+                "T_P": 1e-6,
+                "P_D": 1e-6,
+                "T_DP": 1,
+                "T_CUT": T_CUT,
+            },
+            "num_memories": int(param_matrix[part][1]),
+            "mode": "distribute",
+            "source_position": "central",
+        }
+        for part in range(num_parts)
+    }
+}
+
+
+cases.append(case_specification_1)
+
+case_name = "P_sym150_per_TCUT_1mem_v3"
+num_parts = 64
+max_iter = 10**4
+num_parties = 4
+length = 150e3
+base_params = {
+    "P_LINK": 1,
+    "F_INIT": 0.99,
+    "T_P": 1e-6,  # preparation time, consistent with MHz entangled pair source
+    "P_D": 1e-6,  # dark count probability
+    "T_DP": 1,  # dephasing time
+}
+num_memories = 1
+
+
+
+T_CUTS = np.linspace(0.005, 1, num=num_parts)
+
+case_specification_1 = {
+    "name": case_name,
+    "subcase_name": "distribute_central",
+    "num_parts": num_parts,
+    "index": T_CUTS,
+    "case_args": {
+        part: {
+            "distance_from_central": [length] * int(num_parties-1),
+            "distance_A": length,
+            "num_parties": num_parties,
+            "max_iter": max_iter,
+            "params": {
+                "P_LINK": 1,
+                "F_INIT": 0.99,
+                "T_P": 1e-6,
+                "P_D": 1e-6,
+                "T_DP": 1,
+                "T_CUT": T_CUTS[part],
+            },
+            "num_memories": num_memories,
+            "mode": "distribute",
+            "source_position": "central",
+        }
+        for part in range(num_parts)
+    },
+}
+cases.append(case_specification_1)
+
+case_specification_2 = {
+    "name": case_name,
+    "subcase_name": "distribute_outer",
+    "num_parts": num_parts,
+    "index": T_CUTS,
+    "case_args": {
+        part: {
+            "distance_from_central": [length] * int(num_parties - 1),
+            "distance_A": length,
+            "num_parties": num_parties,
+            "max_iter": max_iter,
+            "params": {
+                "P_LINK": 1,
+                "F_INIT": 0.99,
+                "T_P": 1e-6,
+                "P_D": 1e-6,
+                "T_DP": 1,
+                "T_CUT": T_CUTS[part],
+            },
+            "num_memories": num_memories,
+            "mode": "distribute",
+            "source_position": "outer",
+        }
+        for part in range(num_parts)
+    },
+}
+cases.append(case_specification_2)
+
+
+case_specification_3 = {
+    "name": case_name,
+    "subcase_name": "measure_central",
+    "num_parts": num_parts,
+    "index": T_CUTS,
+    "case_args": {
+        part: {
+            "distance_from_central": [length] * int(num_parties - 1),
+            "distance_A": length,
+            "num_parties": num_parties,
+            "max_iter": max_iter,
+            "params": {
+                "P_LINK": 1,
+                "F_INIT": 0.99,
+                "T_P": 1e-6,
+                "P_D": 1e-6,
+                "T_DP": 1,
+                "T_CUT": T_CUTS[part],
+            },
+            "num_memories": num_memories,
+            "mode": "measure",
+            "source_position": "central",
+        }
+        for part in range(num_parts)
+    },
+}
+cases.append(case_specification_3)
+
+
+case_specification_4 = {
+    "name": case_name,
+    "subcase_name": "measure_outer",
+    "num_parts": num_parts,
+    "index": T_CUTS,
+    "case_args": {
+        part: {
+            "distance_from_central": [length] * int(num_parties - 1),
+            "distance_A": length,
+            "num_parties": num_parties,
+            "max_iter": max_iter,
+            "params": {
+                "P_LINK": 1,
+                "F_INIT": 0.99,
+                "T_P": 1e-6,
+                "P_D": 1e-6,
+                "T_DP": 1,
+                "T_CUT": T_CUTS[part],
+            },
+            "num_memories": num_memories,
+            "mode": "measure",
+            "source_position": "outer",
+        }
+        for part in range(num_parts)
+    },
+}
+cases.append(case_specification_4)
+
+case_name = "P_sym150_per_TCUT_1mem_v4"
+num_parts = 64
+max_iter = 10**4
+num_parties = 4
+length = 150e3
+base_params = {
+    "P_LINK": 1,
+    "F_INIT": 0.99,
+    "T_P": 1e-6,  # preparation time, consistent with MHz entangled pair source
+    "P_D": 1e-6,  # dark count probability
+    "T_DP": 1,  # dephasing time
+}
+num_memories = 1
+
+
+
+T_CUTS = np.linspace(0.05, 2, num=num_parts)
+
+case_specification_1 = {
+    "name": case_name,
+    "subcase_name": "distribute_central",
+    "num_parts": num_parts,
+    "index": T_CUTS,
+    "case_args": {
+        part: {
+            "distance_from_central": [length] * int(num_parties-1),
+            "distance_A": length,
+            "num_parties": num_parties,
+            "max_iter": max_iter,
+            "params": {
+                "P_LINK": 1,
+                "F_INIT": 0.99,
+                "T_P": 1e-6,
+                "P_D": 1e-6,
+                "T_DP": 1,
+                "T_CUT": T_CUTS[part],
+            },
+            "num_memories": num_memories,
+            "mode": "distribute",
+            "source_position": "central",
+        }
+        for part in range(num_parts)
+    },
+}
+cases.append(case_specification_1)
+
+case_name = "P_sym150_per_TCUT_1mem_v5"
+num_parts = 64
+max_iter = 10**4
+num_parties = 4
+length = 150e3
+base_params = {
+    "P_LINK": 1,
+    "F_INIT": 0.99,
+    "T_P": 1e-6,  # preparation time, consistent with MHz entangled pair source
+    "P_D": 1e-6,  # dark count probability
+    "T_DP": 1,  # dephasing time
+}
+num_memories = 1
+
+
+
+T_CUTS = np.linspace(0.05, 5, num=num_parts)
+
+case_specification_1 = {
+    "name": case_name,
+    "subcase_name": "distribute_central",
+    "num_parts": num_parts,
+    "index": T_CUTS,
+    "case_args": {
+        part: {
+            "distance_from_central": [length] * int(num_parties-1),
+            "distance_A": length,
+            "num_parties": num_parties,
+            "max_iter": max_iter,
+            "params": {
+                "P_LINK": 1,
+                "F_INIT": 0.99,
+                "T_P": 1e-6,
+                "P_D": 1e-6,
+                "T_DP": 1,
+                "T_CUT": T_CUTS[part],
+            },
+            "num_memories": num_memories,
+            "mode": "distribute",
+            "source_position": "central",
+        }
+        for part in range(num_parts)
+    },
+}
+cases.append(case_specification_1)
 
 
 num_cases = len(cases)
